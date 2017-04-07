f513209c473dcd0c86c29f7d8bac5327bef298ab
LucasMarcuzzi <lucasmarcuzzi@hotmail.com>
2017-04-07 11:32:22 -0300

    Menu Added
    Added a menu... Duh.

---
+++ TPProgramacion3/TPProgramacion3/Cursor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPProgramacion3
+{
+    class Cursor
+    {
+        private int x;
+        private int y;
+        private char sprite;
+        private int previousy = -1;
+        
+        public Cursor(int posx, int posy, char icon)
+        {
+            x = posx;
+            y = posy;
+            sprite = icon;
+            Draw();
+        }
+        public void MoveUp()
+        {
+            previousy = y;
+            y -= 2;
+            Draw();
+            if(previousy!=-1)
+            CleanOldSprite();
+        }
+        public void MoveDown()
+        {
+            previousy = y;
+            y += 2;
+            Draw();
+            if (previousy != -1)
+            CleanOldSprite();
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sprite);
+            Console.SetCursorPosition(0, 0);
+        }
+        public void CleanOldSprite()
+        {
+            Console.SetCursorPosition(x, previousy);
+            Console.Write(' ');
+            Console.SetCursorPosition(0, 0);
+        }
+        public int GetX()
+        {
+            return x;
+        }
+        public int GetY()
+        {
+            return y;
+        }
+    }
+}
--- TPProgramacion3/TPProgramacion3/Program.cs
+++ TPProgramacion3/TPProgramacion3/Program.cs
@@ -10,8 +10,11 @@ namespace TPProgramacion3
     {
         static void Main(string[] args)
         {
-            bool game = true;
-            Personaje p1;
+            bool menustate = true;
+           bool clearconsole = false; 
+           bool playstate = false;
+           bool game = true;
+           Personaje p1;
            p1 = new Personaje(20, 20, '☺');
            Enemy e1;
            Enemy e2;
@@ -23,118 +26,172 @@ namespace TPProgramacion3
            t1 = new Trap(10, 10, 'X');
            t2 = new Trap(23, 23, 'X');
            t3 = new Trap(30, 15, 'X');
+           Cursor flechita;
+           flechita = new Cursor(32, 15, '→');
             ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
             //COMIENZA EL BUCLE
             ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
            while (game)
            {
-               t1.Draw();
-               t2.Draw();
-               t3.Draw();
-               Console.SetCursorPosition(76, 23);
-               if (Console.KeyAvailable)
+               while (menustate)
                {
+                   if (!clearconsole)
+                   {
+                       Console.Clear();
+                       clearconsole = !clearconsole;
+                       Console.SetCursorPosition(30, 3);
+                       Console.WriteLine("TP Programacion 3");
+                       Console.SetCursorPosition(34, 15);
+                       Console.WriteLine("Empezar");
+                       Console.SetCursorPosition(35, 17);
+                       Console.WriteLine("Salir");
+                       Console.SetCursorPosition(0, 0);
+                       flechita.Draw();
+                   }
                    ConsoleKeyInfo Movimiento = Console.ReadKey();
                    switch (Movimiento.Key)
                    {
                        case ConsoleKey.W:
-                           p1.MoveUp();
+                           if (flechita.GetY() > 15) { 
+                           flechita.MoveUp();
+                   }
+                           Console.SetCursorPosition(0, 0);
                            break;
                        case ConsoleKey.S:
-                           p1.MoveDown();
+                           if (flechita.GetY() < 17)
+                           {
+                               flechita.MoveDown();
+                           }
+                           Console.SetCursorPosition(0, 0);
                            break;
-                       case ConsoleKey.A:
-                           p1.MoveLeft();
+                      case ConsoleKey.Spacebar:
+                           if (flechita.GetY()==15)
+                           {
+                               playstate = true;
+                               menustate = false;
+                               Console.Clear();
+                           }
+                           if (flechita.GetY() == 17)
+                           {
+                               playstate = false;
+                               menustate = false;
+                               game = false;
+                           }
                            break;
-                       case ConsoleKey.D:
-                           p1.MoveRight();
-                           break;
-                       case ConsoleKey.P:
-                           game = !game;
+                       default:
+                           Console.SetCursorPosition(0, 0);
                            break;
                    }
-                   p1.CleanLastSprite();
                }
-               ///////////////////////////////////////////////////////////////////////////////////////////////////////
-               //INTERACCIONES ENTRE ENTIDADES
-               ///////////////////////////////////////////////////////////////////////////////////////////////////////
-               if (e1.GetX() > p1.GetX())
+               while (playstate)
                {
-                   e1.GoLeft();
+                   t1.Draw();
+                   t2.Draw();
+                   t3.Draw();
+                   Console.SetCursorPosition(76, 23);
+                   if (Console.KeyAvailable)
+                   {
+                       ConsoleKeyInfo Movimiento = Console.ReadKey();
+                       switch (Movimiento.Key)
+                       {
+                           case ConsoleKey.W:
+                               p1.MoveUp();
+                               break;
+                           case ConsoleKey.S:
+                               p1.MoveDown();
+                               break;
+                           case ConsoleKey.A:
+                               p1.MoveLeft();
+                               break;
+                           case ConsoleKey.D:
+                               p1.MoveRight();
+                               break;
+                           case ConsoleKey.P:
+                               game = !game;
+                               break;
+                       }
+                       p1.CleanLastSprite();
+                   }
+                   ///////////////////////////////////////////////////////////////////////////////////////////////////////
+                   //INTERACCIONES ENTRE ENTIDADES
+                   ///////////////////////////////////////////////////////////////////////////////////////////////////////
+                   if (e1.GetX() > p1.GetX())
+                   {
+                       e1.GoLeft();
+                   }
+                   if (e1.GetX() < p1.GetX())
+                   {
+                       e1.GoRight();
+                   }
+                   if (e1.GetY() > p1.GetY())
+                   {
+                       e1.GoUp();
+                   }
+                   if (e1.GetY() < p1.GetY())
+                   {
+                       e1.GoDown();
+                   }
+                   if (e2.GetX() > p1.GetX())
+                   {
+                       e2.GoLeft();
+                   }
+                   if (e2.GetX() < p1.GetX())
+                   {
+                       e2.GoRight();
+                   }
+                   if (e2.GetY() > p1.GetY())
+                   {
+                       e2.GoUp();
+                   }
+                   if (e2.GetY() < p1.GetY())
+                   {
+                       e2.GoDown();
+                   }
+                   if ((e1.GetX() == p1.GetX()) && (e1.GetY() == p1.GetY()))
+                   {
+                       p1.Respawn();
+                   }
+                   if ((e2.GetX() == p1.GetX()) && (e2.GetY() == p1.GetY()))
+                   {
+                       p1.Respawn();
+                   }
+                   if (t1.Overlap(p1.GetX(), p1.GetY()))
+                   {
+                       p1.Respawn();
+                   }
+                   if (t1.Overlap(e1.GetX(), e1.GetY()))
+                   {
+                       e1.Respawn();
+                   }
+                   if (t1.Overlap(e2.GetX(), e2.GetY()))
+                   {
+                       e2.Respawn();
+                   } if (t2.Overlap(p1.GetX(), p1.GetY()))
+                   {
+                       p1.Respawn();
+                   }
+                   if (t2.Overlap(e1.GetX(), e1.GetY()))
+                   {
+                       e1.Respawn();
+                   }
+                   if (t2.Overlap(e2.GetX(), e2.GetY()))
+                   {
+                       e2.Respawn();
+                   }
+                   if (t3.Overlap(p1.GetX(), p1.GetY()))
+                   {
+                       p1.Respawn();
+                   }
+                   if (t3.Overlap(e1.GetX(), e1.GetY()))
+                   {
+                       e1.Respawn();
+                   }
+                   if (t3.Overlap(e2.GetX(), e2.GetY()))
+                   {
+                       e2.Respawn();
+                   }
+                   System.Threading.Thread.Sleep(80);
                }
-               if (e1.GetX() < p1.GetX())
-               {
-                   e1.GoRight();
-               }
-               if (e1.GetY() > p1.GetY())
-               {
-                   e1.GoUp();
-               }
-               if (e1.GetY() < p1.GetY())
-               {
-                   e1.GoDown();
-               }
-               if (e2.GetX() > p1.GetX())
-               {
-                   e2.GoLeft();
-               }
-               if (e2.GetX() < p1.GetX())
-               {
-                   e2.GoRight();
-               }
-               if (e2.GetY() > p1.GetY())
-               {
-                   e2.GoUp();
-               }
-               if (e2.GetY() < p1.GetY())
-               {
-                   e2.GoDown();
-               }
-               if ((e1.GetX() == p1.GetX()) && (e1.GetY() == p1.GetY()))
-               {
-                   p1.Respawn();
-               }
-               if ((e2.GetX() == p1.GetX()) && (e2.GetY() == p1.GetY()))
-               {
-                   p1.Respawn();
-               }
-               if (t1.Overlap(p1.GetX(), p1.GetY()))
-               {
-                   p1.Respawn();
-               }
-               if (t1.Overlap(e1.GetX(), e1.GetY()))
-               {
-                   e1.Respawn();
-               }
-               if (t1.Overlap(e2.GetX(), e2.GetY()))
-               {
-                   e2.Respawn();
-               } if (t2.Overlap(p1.GetX(), p1.GetY()))
-               {
-                   p1.Respawn();
-               }
-               if (t2.Overlap(e1.GetX(), e1.GetY()))
-               {
-                   e1.Respawn();
-               }
-               if (t2.Overlap(e2.GetX(), e2.GetY()))
-               {
-                   e2.Respawn();
-               }
-               if (t3.Overlap(p1.GetX(), p1.GetY()))
-               {
-                   p1.Respawn();
-               }
-               if (t3.Overlap(e1.GetX(), e1.GetY()))
-               {
-                   e1.Respawn();
-               }
-               if (t3.Overlap(e2.GetX(), e2.GetY()))
-               {
-                   e2.Respawn();
-               }
-               System.Threading.Thread.Sleep(80);
-               
            }
         }
     }
